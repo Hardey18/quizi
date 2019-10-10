79e75993d2b9a9744afcc3b7790037f8c9e309b2
Adewale Nurudeen <nurudeenadeolaa@gmail.com>
2019-10-10 01:35:26 -0700

    Initial commit

---
+++ public/main.js
@@ -0,0 +1,79 @@
+$(function (){
+
+    var $orders = $('#orders');
+    var $question = $('#question');
+    var $option1 = $('#option1');
+    var $option2 = $('#option2');
+    var $option3 = $('#option3');
+    var $option4 = $('#option4');
+
+    var orderTemplate = "" + 
+    "<li class=top>" + 
+    "<h4>{{question}}</h4>" + "<br>" +
+    "<button>{{option1}}</button>" + "<br>" +
+    "<button>{{option2}}</button>" + "<br>" +
+    "<button>{{option3}}</button>" + "<br>" +
+    "<button>{{option4}}</button>" + "<br>" +
+    "<button data-id='{{id}}' class='remove'>X</button>" +
+    "</li>";
+
+    // "<li class=top>" + 
+    // "<p><strong>NAME:</strong> {{name}}</p>" + 
+    // "<p><strong>DRINK:</strong> {{drink}}</p>" + 
+    // "<button data-id='{{id}}' class='remove'>X</button>" +
+    // "</li>";
+
+    function addOrder(order) {
+        $orders.append(Mustache.render(orderTemplate, order));
+    }
+
+    $.ajax({
+        type: 'GET',
+        url: 'order',
+        success: function(orders) {
+            $.each(orders, function(i, order) {
+                addOrder(order)
+            });
+        },
+        error: function() {
+            alert('error loading orders');
+        }
+    });
+
+    $('#add-order').on('click', function() {
+        var order = {
+            question: $question.val(),
+            option1: $option1.val(),
+            option2: $option2.val(),
+            option3: $option3.val(),
+            option4: $option4.val(),
+        };
+
+        $.ajax({
+            type: 'POST',
+            url: 'order',
+            data: order,
+            success: function(newOrder) {
+                addOrder(newOrder);
+            },
+            error: function() {
+                alert('error saving order');
+            }
+        });
+        $('button').after('<p>Order added successfully!</p>')
+    });
+
+    $orders.delegate('.remove', 'click', function() {
+        var $li = $(this).closest('li');
+
+        $.ajax({
+            type: 'DELETE',
+            url: 'order' + "/" + $(this).attr('data-id'),
+            success: function (){
+                $li.fadeOut(300, function() {
+                    $(this).remove();
+                });
+            }
+        });
+    });
+});
